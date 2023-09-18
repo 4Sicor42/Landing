5c378ceaf86db731e16e30dd8e98087ad00ecc6f
Sicor <atrem641@gmail.com>
2023-09-18 21:54:02 +0300

    feat: addet player and translate

---
+++ script.js
@@ -0,0 +1,274 @@
+const i18Obj = {
+  en: {
+    theme:"Select theme ->",
+    realco: "Realco - Аренда",
+    lang: "Select a language -> ",
+    navbutthome: "Home",
+    navbuttprop: "Propertis",
+    navbuttabout: "About",
+    navbuttcontact: "Contacts",
+    navbuttsrch: "Search",
+    navbuttcart: "Cart",
+    sprop: "Search properties",
+    navbuttfind: "Find a property",
+    dreampalace: "Find your dream place",
+    findhouse: "Find house for your family in minutes",
+    pain: "Pain should be followed by the client's education.",
+    aenean: "Aenean members of the country are easy to find, even if they are big. Sometimes it will be necessary to take care of the needs of some people in pure mauris.",
+    worldbest: "As seen on the world &#x27;s best magazines and media",
+    works: "How it works",
+    now: "Now the cost is now at the cost. It's something that happens to my kids. Maecenas is a casino player.",
+    property: "Find property",
+    oda: "Odales mauris who is easy land, or sometimes large land. Chat needs some support. In pure mauris, the author is not a teenager.",
+    deal: "Make a deal",
+    child: "Children are welcome. For it was said, that he was from the end of his life, an urn, except for the care of the doctor, with easy arrows, the torturer and me hated him.",
+    keys: "Get your keys",
+    alco: "There is no alcohol to drink. It's time to make up a pillow. Always at home, the fear of life is a result.",
+    comfort: "Comfort first",
+    best: "The best houses for family comfort",
+    ijust: "I just hit his throat, and always like the players at the level. For this reason, it is recommended not to put it on the internet.",
+    explore: "Explore properties",
+    agent: "Contact agent",
+    quick: "Quick and easy",
+    fast: "Fast forward process",
+    as: "As in life, except for football, the most important sauce and that's pure. Let it be that my bow is present, and that no trigger is played.",
+    prices: "Affordable prices",
+    customer: "The customer is very important, the customer will be followed by the customer. In fact, the casino is in the eros.",
+    less: "Less paper work",
+    curabitur: "Curabitur is always a porter of mourning until it needs a policy of time and ends.",
+    prop: "Properties",
+    houses: "Houses in your favorite area",
+    cozy: "Cozy studio in Los Angeles",
+    tiny: "Tiny home in San Diego",
+    crown: "Crown house in New York",
+    family: "Family house in Los Angeles",
+    studio: "Studio in Las Vegas",
+    san: "San Diego story",
+    sqrt: "sqrt",
+    pages: "Pages",
+    about: "About",
+    agents: "Agents",
+    faqs: "FAQs",
+    home: "Home",
+    blog: "Blog",
+    contact: "Contact",
+    properties: "Properties",
+    cms: "CMS Pages",
+    property: "Property",
+    blogc: "Blog Categories",
+    blogS: "Blog Single",
+    agentS: "Agent Single",
+    utility: "Utility",
+    pages: "Pages",
+    style: "Style",
+    guide: "Guide",
+    changelog: "Changelog",
+    licenses: "Licenses",
+    password: "Password",
+    search: "Search",
+    subscribe: "Subscribe",
+    join: "Join our newsletter to stay up to date on features and releases.",
+    by: "By subscribing you agree to with our Privacy Policy and provide consent to receive updates from our company.",
+    fhome:"Home",
+    fabout:"About",
+    fblog:"Blog",
+    fagent:"Agents",
+    fcontact:"Contacts",
+    ffaq:"FAQs",
+    fprop:"Properties",
+    foth:"Other",
+    CMS:"CMS pages",
+    fprope:"Property",
+    fpropes:"Property Single",
+    fpropec:"Blog Categories",
+    fps:"Blog Single",
+    fas:"Agent Single",
+    up:"Utility Pages",
+    fsg:"Style Guide",
+    fcl:"Changelog",
+    fl:"Licenses",
+    ferr:"404",
+    fpasw:"Password",
+    fshrc:"Search",
+    fsbsc:"Subscribe",
+    fjoin:"Join our newsletter to stay up to date on features and releases.",
+    fby:"By subscribing you agree to with our",
+    fpp:"Privacy Policy",
+    fandp:"and provide consent to receive updates from our company.",
+    fRealco:"© Realco by ",
+    fms:"Minimal Square",
+    fpow:"Powered by Realco ."
+  },
+  ru: {
+    theme: "Выберите тему ->",
+    realco: "Realco - Аренда",
+    lang: "Выберите язык ->",
+    navbutthome: "Домой",
+    navbuttprop: "Свойства",
+    navbuttabout: "О нас",
+    navbuttcontact: "Контакты",
+    navbuttsrch: "Поиск",
+    navbuttcart: "Корзина",
+    sprop: "Поиск недвижимости",
+    navbuttfind: "Найти недвижимость",
+    dreampalace: "Найдите свое место мечты",
+    findhouse: "Найдите дом для вашей семьи в течение нескольких минут",
+    pain: "Боль должна сопровождаться образованием клиента.",
+    aenean: "Энейских представителей страны легко найти, даже если они большие. Иногда потребуется заботиться о нуждах некоторых людей в чистом мавре.",
+    worldbest: "Как видно в лучших мировых журналах и СМИ",
+    works: "Как это работает",
+    now: "Теперь стоимость стоит так же. Это что-то, что случается с моими детьми. Маэценас - игрок в казино.",
+    property: "Найти недвижимость",
+    oda: "Одальс Маурис, которому легко земля, иногда даже большая. Чату нужна поддержка. В чистом мавре автор не подросток.",
+    deal: "Заключить сделку",
+    child: "Дети приветствуются. Говорили, что он был урной с конца своей жизни, за исключением ухода врача, с легкими стрелами, палачом, и мной, который его ненавидел.",
+    keys: "Получите ваши ключи",
+    alco: "Алкоголя нет. Пора подложить подушку. Всегда дома, страх перед жизнью - это результат.",
+    comfort: "Комфорт на первом месте",
+    best: "Лучшие дома для семейного комфорта",
+    ijust: "Я только что ударил его в горло, и всегда как игроки на высшем уровне. По этой причине рекомендуется не размещать это в интернете.",
+    explore: "Исследование недвижимости",
+    agent: "Свяжитесь с агентом",
+    quick: "Быстро и легко",
+    fast: "Быстрый процесс",
+    as: "Как в жизни, кроме футбола, самый важный соус, и это чисто. Пусть мой лук будет присутствовать, и не будет срабатывать ни один спусковой крючок.",
+    prices: "Доступные цены",
+    customer: "Клиент очень важен, клиента будет сопровождать клиент. Фактически, казино находится в эросе.",
+    less: "Меньше бумажной работы",
+    curabitur: "Курабитур всегда носитель траура, пока не потребуется политика времени и конец.",
+    prop: "Недвижимость",
+    houses: "Дома в вашем любимом районе",
+    cozy: "Уютная студия в Лос-Анджелесе",
+    tiny: "Крошечный дом в Сан-Диего",
+    crown: "Королевский дом в Нью-Йорке",
+    family: "Семейный дом в Лос-Анджелесе",
+    studio: "Студия в Лас-Вегасе",
+    san: "История Сан-Диего",
+    sqrt: "кв. корень",
+    pages: "Страницы",
+    about: "О нас",
+    agents: "Агенты",
+    faqs: "Часто задаваемые вопросы",
+    home: "Главная",
+    blog: "Блог",
+    contact: "Контакты",
+    properties: "Недвижимость",
+    cms: "Страницы CMS",
+    property: "Собственность",
+    blogc: "Категории блога",
+    blogS: "Один блог",
+    agentS: "Один агент",
+    utility: "Утилита",
+    pages: "Страницы",
+    style: "Стиль",
+    guide: "Руководство",
+    changelog: "История изменений",
+    licenses: "Лицензии",
+    password: "Пароль",
+    search: "Поиск",
+    subscribe: "Подписаться",
+    join: "Присоединяйтесь к нашему бюллетеню, чтобы быть в курсе новых функций и выпусков.",
+    by: "Подписываясь, вы соглашаетесь с нашей политикой конфиденциальности и даёте согласие на получение обновлений от нашей компании.",
+    fhome: "Главная",
+    fabout: "О нас",
+    fblog: "Блог",
+    fagent: "Агенты",
+    fcontact: "Контакты",
+    ffaq: "Часто задаваемые вопросы",
+    fprop: "Недвижимость",
+    foth: "Другое",
+    CMS: "Страницы CMS",
+    fprope: "Собственность",
+    fpropes: "Один объект недвижимости",
+    fpropec: "Категории блога",
+    fps: "Один блог",
+    fas: "Один агент",
+    up: "Утилитарные страницы",
+    fsg: "Руководство по стилю",
+    fcl: "История изменений",
+    fl: "Лицензии",
+    ferr: "404",
+    fpasw: "Пароль",
+    fshrc: "Поиск",
+    fsbsc: "Подписаться",
+    fjoin: "Присоединяйтесь к нашему бюллетеню, чтобы быть в курсе новых функций и выпусков.",
+    fby: "Подписываясь, вы соглашаетесь с нашей",
+    fpp: "Политикой конфиденциальности",
+    fandp: "и даёте согласие на получение обновлений от нашей компании.",
+    fRealco: "© Realco от ",
+    fms: "Минимальный квадрат",
+    fpow: "Работает на платформе Realco."
+  }
+}
+
+var btn = document.getElementById("dark-theme-button");
+var link = document.getElementById("theme-link");
+
+btn.addEventListener("click", function () {
+  ChangeTheme();
+});
+
+function setTheme(theme) {
+  localStorage.setItem("theme", theme);
+}
+
+function applyThemeOnLoad() {
+  const storedTheme = localStorage.getItem("theme");
+  const storedLanguage = localStorage.getItem("language");
+  if (storedTheme) {
+    const link = document.getElementById("theme-link"); 
+    link.setAttribute("href", storedTheme);
+  }
+  if (storedLanguage) {
+    // Примените язык к текстовым элементам на странице
+    applyLanguage(storedLanguage);
+  }
+}
+
+function applyLanguage(language) {
+  const elementsToTranslate = document.querySelectorAll("[data-i18]");
+  
+  elementsToTranslate.forEach((element) => {
+    const key = element.getAttribute("data-i18");
+    element.textContent = i18Obj[language][key] || "";
+  });
+
+  // Сохраните выбранный язык в локальном хранилище
+  localStorage.setItem("language", language);
+}
+
+
+document.getElementById("languageButtonEN").addEventListener("click", function () {
+  applyLanguage("en");
+});
+
+document.getElementById("languageButtonRU").addEventListener("click", function () {
+  applyLanguage("ru");
+});
+
+
+
+
+function ChangeTheme() {
+  let lightTheme = "./style.css";
+  let darkTheme = "./dark.css";
+
+  var currTheme = link.getAttribute("href");
+  var theme = "";
+
+  if (currTheme == lightTheme) {
+    currTheme = darkTheme;
+    theme = "dark";
+  } else {
+    currTheme = lightTheme;
+    theme = "light";
+  }
+
+  setTheme(currTheme); 
+  link.setAttribute("href", currTheme);
+}
+
+applyThemeOnLoad();
+
+
+
